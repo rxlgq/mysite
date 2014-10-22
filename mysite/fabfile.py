2d4919116fc69494ce32b6a2983f8eb13043e398
lgq <guangquanl@sina.com>
2014-10-22 11:36:42 +0800

    This is a demo.

---
+++ mysite/fabfile.py
@@ -0,0 +1,29 @@
+
+
+'''
+Created on Oct 21, 2014
+
+@author: lgq
+'''
+# from fabric.api import lcd, local
+# 
+# def prepare_deployment(branch_name):
+#     local('python manage.py test mysite')
+#     local('git add -p && git commit') # or local('hg add && hg commit')
+#     
+# def deploy():
+#     with lcd('/path/to/my/prod/area/'):
+#  
+#         # With git...
+#         local('git pull /my/path/to/dev/area/')
+#  
+#         # With Mercurial...
+# #         local('hg pull /my/path/to/dev/area/')
+# #         local('hg update')
+#  
+#         # With both
+#         local('python manage.py migrate myapp')
+#         local('python manage.py test myapp')
+#         local('/my/command/to/restart/webserver')
+        
+        
